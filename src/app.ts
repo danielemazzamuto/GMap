1a29215291cb1953d958fdee4a022fa926aa7cc3
danielemazzamuto <mazza.92@hotmail.it>
2024-04-04 23:19:42 +0200

    Axios and GoogleMaps API

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,45 @@
-// Code goes here!
+import axios from 'axios';
+
+const form = document.querySelector('form')!;
+const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+const GOOGLE_API_KEY = 'AIzaSyBYsmHwGQhXDdjwRTAVoiNwuCAWe6t8294';
+
+// Google Geocoding API response type
+type GoogleGeocodingResponse = {
+  results: {geometry: {location: {lat: number, lng: number}}}[];
+  status: 'OK' | 'ZERO_RESULTS';
+}
+
+declare var google: any;
+
+function searchAddressHandler(event: Event) {
+  event.preventDefault();
+  const enteredAddress = addressInput.value;
+
+  // send this to Google's API
+  axios.get<GoogleGeocodingResponse>(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(enteredAddress)}&key=${GOOGLE_API_KEY}`)
+    .then(response => {
+      if(response.data.status !== 'OK')
+        throw new Error('Could not fetch location!');
+
+      const { lat, lng } = response.data.results[0].geometry.location;
+      
+      const map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat, lng},
+        zoom: 16
+      });
+      
+      new google.maps.Marker({
+        position: {lat, lng},
+        map,
+      });
+
+    })
+    .catch(err => {
+      alert(err.message);
+      console.log(err);
+    });
+}
+
+form.addEventListener('submit', searchAddressHandler);
