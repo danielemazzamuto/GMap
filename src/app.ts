43d5b619ee836b2373de29b211957553b5aac9ab
Daniele Mazzamuto <daniele.mazzamuto@tui.com>
2024-04-23 12:25:05 +0200

    Style and .env var

--- src/app.ts
+++ src/app.ts
@@ -3,13 +3,11 @@ import axios from 'axios';
 const form = document.querySelector('form')!;
 const addressInput = document.getElementById('address')! as HTMLInputElement;
 
-const GOOGLE_API_KEY = 'AIzaSyBYsmHwGQhXDdjwRTAVoiNwuCAWe6t8294';
-
 // Google Geocoding API response type
 type GoogleGeocodingResponse = {
-  results: {geometry: {location: {lat: number, lng: number}}}[];
+  results: { geometry: { location: { lat: number; lng: number } } }[];
   status: 'OK' | 'ZERO_RESULTS';
-}
+};
 
 // declare google object to avoid typescript error, as google is not a module
 declare var google: any;
@@ -19,25 +17,29 @@ function searchAddressHandler(event: Event) {
   const enteredAddress = addressInput.value;
 
   // send this to Google's API
-  axios.get<GoogleGeocodingResponse>(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(enteredAddress)}&key=${GOOGLE_API_KEY}`)
-    .then(response => {
-      if(response.data.status !== 'OK')
+  axios
+    .get<GoogleGeocodingResponse>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress
+      )}&key=${process.env.GOOGLE_API_KEY}`
+    )
+    .then((response) => {
+      if (response.data.status !== 'OK')
         throw new Error('Could not fetch location!');
 
       const { lat, lng } = response.data.results[0].geometry.location;
-      
+
       const map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat, lng},
-        zoom: 16
-      });
-      
-      new google.maps.Marker({
-        position: {lat, lng},
-        map,
+        center: { lat, lng },
+        zoom: 16,
       });
 
+      new google.maps.Marker({
+        position: { lat, lng },
+        map,
+      });
     })
-    .catch(err => {
+    .catch((err) => {
       alert(err.message);
       console.log(err);
     });
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   mode: 'development',
@@ -26,5 +27,8 @@ module.exports = {
   },
   resolve: {
     extensions: ['.ts', '.js']
-  }
+  },
+  plugins: [
+    new Dotenv(),
+  ]
 };
